91d96e6787991630bc0683694f6d7a664657499f
Hossam Medhat Shokry <hossammedhat81@gmail.com>
2025-09-19 07:46:54 +0300

    move files into src

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 load_dotenv(".env")  # Load environment variables from .env file
 
-from routes import base
+from src import base
 
 app = FastAPI()
 
