e492e3a308f49bed63a049acc5a246727995ed2d
Hossam Medhat Shokry <hossammedhat81@gmail.com>
2025-09-25 21:33:17 +0300

    now we can chunk files with types pdf,txt

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -25,6 +25,9 @@ class ProcessController(BaseController):
             file_id
         )
 
+        if not os.path.exists(file_path):
+            return None
+
         if file_ext == ProcessingEnum.TXT.value:
             return TextLoader(file_path, encoding="utf-8")
 
@@ -34,10 +37,12 @@ class ProcessController(BaseController):
         return None
 
     def get_file_content(self, file_id: str):
+
         loader = self.get_file_loader(file_id=file_id)
-        content = loader.load()
-        print(f"Loaded content for {file_id}: {content}")  # Add this line for debugging
-        return content
+        if loader:
+            return loader.load()
+
+        return None
 
     def process_file_content(self, file_content: list, file_id: str,
                             chunk_size: int=100, overlap_size: int=20):
